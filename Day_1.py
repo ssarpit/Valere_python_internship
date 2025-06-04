cd7d5d1a5a90be87fd2a0374f0b504ddb0a07f85
ssarpit <arpitofficial0206@gmail.com>
2025-06-04 18:10:37 +0530

    Update with Pep8

--- Day_1.py
+++ Day_1.py
@@ -1,28 +1,28 @@
 # #VARIABLES AND DATA TYPES
+# # a=2
+# # print(type(a))
+# name="arpit jain "
+# print(type(name))
+# b=2.3
+# print(type(b))
+# is_developer=True
+# print(type(is_developer))
+
+# # #INPUT AND OUTPUT IN PYTHON
+# num1=(input("enter name "))
+# print(num1)
+
+
+# # #OPERATORS IN PYTHON
 # a=2
-# print(type(a))
-name="arpit jain "
-print(type(name))
-b=2.3
-print(type(b))
-is_developer=True
-print(type(is_developer))
-
-# #INPUT AND OUTPUT IN PYTHON
-num1=(input("enter name "))
-print(num1)
-
-
-# #OPERATORS IN PYTHON
-a=2
-b=3
-print(a+b)
-print(a*b)
-print(a-b)
-print(a/b)
-print(a%b)
-print(a>=b)
-print(a<=b)
+# b=3
+# print(a+b)
+# print(a*b)
+# print(a-b)
+# print(a/b)
+# print(a%b)
+# print(a>=b)
+# print(a<=b)
 
 #SIMPLE CALCULATOR IN PYTHON
 
@@ -36,22 +36,24 @@ elif op=="-":
 elif op=="*":
     print(f"Result:{num1*num2}")
 elif op=="/":
-    if num2!=0:
-        print(f"Result: {num1/num2}")
-    else:
-        print(f"DIVISION OF NUMBER BY ZERO GIVES INFINITY ")
+    try:
+        result=num1/num2
+        print(f"Result: {result}")
+    except Exception as e:
+        print(e)
 elif op=="%":
-    if num2!=0:
-        print(f"Result of modulo operator:{num1%num2}")
-    else:
+    try:
+        result=num1%num2
+        print(f"Result is: {result}")
+    except ZeroDivisionError:
         print(f"Modulo operator does not work for zero denominator ")
 else:
     print("INVALID OPERATION ")
 
 # calculator without using if else statement
-num1 = float(input("enter number_1 "))
-num2 = float(input("enter number_2 "))
-print(num1+num2)
-print(num1-num2)
-print(num1*num2)
-print(num1/num2)
+# num1 = float(input("enter number_1 "))
+# num2 = float(input("enter number_2 "))
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+ a=2
+# # print(type(a))
+# name="arpit jain "
+# print(type(name))
+# b=2.3
+# print(type(b))
+# is_developer=True
+# print(type(is_developer))
+
+# # #INPUT AND OUTPUT IN PYTHON
+# num1=(input("enter name "))
+# print(num1)
+
+
+# # #OPERATORS IN PYTHON
+# a=2
+# b=3
+# print(a+b)
+# print(a*b)
+# print(a-b)
+# print(a/b)
+# print(a%b)
+# print(a>=b)
+# print(a<=b)
